8951b4bf9d1b5771132e7fa2caf5d9b0208cf2b5
Zhuzhiuo <zhiguo_zhu@c-sky.com>
2018-07-10 17:34:59 +0800

    cleanup code

--- kernel/vcall/vcall.mk
+++ kernel/vcall/vcall.mk
@@ -18,13 +18,6 @@ endif
 
 vcall ?= rhino
 
-ifeq ($(vcall),freertos)
-GLOBAL_DEFINES += VCALL_FREERTOS
-
-$(NAME)_SOURCES += \
-    aos/aos_freertos.c
-endif
-
 ifeq ($(vcall),posix)
 GLOBAL_DEFINES += VCALL_POSIX
 
@@ -32,35 +25,9 @@ $(NAME)_SOURCES += \
     aos/aos_posix.c
 endif
 
-ifeq ($(vcall),rhino_sxr)
-GLOBAL_DEFINES += VCALL_SXR
-
-$(NAME)_SOURCES += \
-    aos/aos_sxr.c
-endif
-
-ifeq ($(vcall),rhino_sxr_mutios)
-GLOBAL_DEFINES += VCALL_SXR_MUTIOS
-
-$(NAME)_SOURCES += \
-    aos/aos_rhino_rda8955.c
-endif
-
 ifeq ($(vcall),rhino)
 GLOBAL_DEFINES += VCALL_RHINO
 $(NAME)_COMPONENTS += rhino
-
-ifeq ($(HOST_MCU_FAMILY),esp32)
-$(NAME)_COMPONENTS += vcall.espos
-else
-ifeq ($(HOST_MCU_FAMILY),esp8266)
-$(NAME)_COMPONENTS += vcall.espos
-endif
-endif
-
-ifneq (,$(filter $(PLATFORM), linuxhost mk3060 mk3239 mk3166 mk3165))
-$(NAME)_SOURCES += mico/mico_rhino.c
-endif
 
 $(NAME)_SOURCES += \
     aos/aos_rhino.c
